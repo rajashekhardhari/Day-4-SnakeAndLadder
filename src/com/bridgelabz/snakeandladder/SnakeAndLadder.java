4faf13df05c718f3c33eb65a3cece825b09b11c1
srajashekhar <udayarajashekhar@gmail.com>
2022-06-13 15:25:11 +0530

    Added:The Player gets to exact winning position 100

--- src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
+++ src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
@@ -4,40 +4,44 @@ import java.util.Random;
 
 public class SnakeAndLadder {
 
-	int noPlay = 0;
-	int ladder = 1;
-	int snake = 2;
-
 	public static void main(String[] args) {
 		System.out.println("Welcome to Snake and Ladder Program");
 
 		int startPosition = 0;
-		Random randomNum = new Random();
-		int dice = randomNum.nextInt(6) + 1;
-		System.out.println("Dice Number:-" + dice);
 
 		int noPlay = 0;
 		int ladder = 1;
 		int snake = 2;
+		int maxPosition = 100;
+		int minPosition = 0;
 
-		int checkPlay = (int) Math.floor(Math.random() * 10) % 3;
-		System.out.println("Random Number:-" + checkPlay);
+		while (startPosition < maxPosition) {
 
-		switch (checkPlay) {
-		case 0:
-			break;
+			Random randomNum = new Random();
+			int dice = randomNum.nextInt(6) + 1;
+			System.out.println("Dice Number:-" + dice);
 
-		case 1:
-			startPosition += dice;
-			break;
+			int checkPlay = (int) Math.floor(Math.random() * 10) % 3;
+			System.out.println("Random Number:-" + checkPlay);
 
-		case 2:
-			startPosition -= dice;
-			break;
+			switch (checkPlay) {
+			case 0:
+				break;
+
+			case 1:
+				startPosition += dice;
+				break;
+
+			case 2:
+				startPosition -= dice;
+				if (startPosition < minPosition)
+					startPosition = 0;
+				break;
+			}
+
+			System.out.println("Start Position:-" + startPosition);
+
 		}
 
-		System.out.println("Start Position:-" + startPosition);
-
 	}
-
 }
